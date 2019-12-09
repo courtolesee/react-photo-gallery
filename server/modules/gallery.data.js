13d0452ba214ebe5b71de771d4b90d7818aa98e2
Courtney Olesee <court.olson1@gmail.com>
2019-12-09 08:54:19 -0600

    final css tweaks(?!)

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,23 +1,20 @@
 const galleryItems = [
-    { id: 1, path: 'images/meteora.jpg', description: 'Photo taken during Meteora scramble hike', likes: 0 },
-    { id: 9, path: 'images/tettegoucheRockjpg.jpg', description: 'Photo from a hike at Tettegouche State Park - the sea stack that no longer exists :(', likes: 0 },
-    { id: 2, path: 'images/fall.jpg', description: 'Photo of one of my favorite overlooks on the Superior Hiking Trail', likes: 0 },
-    { id: 3, path: 'images/kitchen.jpg', description: 'Photo of my kitchen after renovation', likes: 0 },
-    { id: 5, path: 'images/odin.jpg', description: 'Photo of my dog, Odin', likes: 0 },
-    { id: 6, path: 'images/yoshi.jpg', description: 'Photo of my dog, Yoshi', likes: 0 },
+    { id: 1, path: 'images/meteora.jpg', description: 'Photo taken during Meteora (Greece) scramble hike', likes: 0 },
+    { id: 9, path: 'images/tettegoucheRockjpg.jpg', description: 'Photo from a hike at Tettegouche State Park - the sea stack that no longer exists after a December 2019 wind/snow storm :(', likes: 0 },
+    { id: 3, path: 'images/kitchen.jpg', description: 'Photo of my kitchen after I renovated it - my biggest project yet!', likes: 0 },
     { id: 7, path: 'images/TempleOfAthenaNike.jpeg', description: 'Photo from a visit to Temple of Athena Nike in Athens, Greece', likes: 0 },
     { id: 8, path: 'images/lakeSuperiorKayak.jpg', description: 'Photo from kayaking on Lake Superior', likes: 0 },
-    { id: 10, path: 'images/AkiceCave.jpeg', description: 'Photo of exploring ice caves in Juneau, AK', likes: 0 },
+    { id: 10, path: 'images/AkiceCave.jpeg', description: 'Photo of me exploring ice caves in Juneau, AK', likes: 0 },
     { id: 11, path: 'images/FijiOverlook.jpg', description: 'Photo from a visit to Kadavu Island, Fiji', likes: 0 },
     { id: 12, path: 'images/meteora1.jpeg', description: 'Photo from a trip to Meteora, Greece -  one of the floating monasteries', likes: 0 },
     { id: 13, path: 'images/meteora2.jpeg', description: 'Photo from a trip to Meteora, Greece -  another of the floating monasteries', likes: 0 },
-    { id: 14, path: 'images/TheParthenon.jpeg', description: 'Photo from a trip to Athens, Greece - The Parthenon on the Athenian Acropolis', likes: 0 },
     { id: 15, path: 'images/YoshiOverlook.jpeg', description: 'Yoshi and I near the Castle Danger Superior Hiking Trail entry point', likes: 0 },
+    { id: 2, path: 'images/fall.jpg', description: 'Photo of one of my favorite overlooks on the Superior Hiking Trail (East Castle Danger entry point)', likes: 0 },
+    { id: 5, path: 'images/odin.jpg', description: 'Photo of my dog, Odin', likes: 0 },
+    { id: 20, path: 'images/Yoshi3.jpg', description: 'My dog Yoshi and I', likes: 0 },
     { id: 16, path: 'images/NewOrleans.jpeg', description: 'New Orleans boil with my sisters', likes: 0 },
     { id: 17, path: 'images/SandFlyNZ.jpg', description: 'From a trip to Sandfly Beach near Dunedin, New Zealand', likes: 0 },
     { id: 18, path: 'images/OdeonofHerodesAtticus.jpeg', description: 'The Odeon of Herodes Atticus where I saw the Greek Orchestra', likes: 0 },
-    { id: 19, path: 'images/meteora3.jpeg', description: 'Photo taken during Meteora scramble hike', likes: 0 },
-    { id: 20, path: 'images/Yoshi3.jpg', description: 'Yoshi and I', likes: 0 }
 ];
 
 
